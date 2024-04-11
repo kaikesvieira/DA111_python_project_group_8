eb22b60fcf75c251f16530c639ce9d7c39894129
kaikesvieira <kaikeincanada@gmail.com>
2024-04-11 12:32:51 -0400

    Working model

--- Website/website.py
+++ Website/website.py
@@ -21,10 +21,11 @@ def about():
 def data():
     con = sqlite3.connect('Trees.db')
     cursor = con.cursor()
-    trees = cursor.execute("SELECT * FROM trees").fetchall()
+    trees = cursor.execute("SELECT *, AVG(DBH) AS avg_dbh FROM trees GROUP BY Species ORDER BY avg_dbh DESC LIMIT 20").fetchall()
+    trees_sorted = sorted(trees, key=lambda x: x[4], reverse=True)
     con.close()
 
-    return render_template("data_table.html", trees = trees)
+    return render_template("data_table.html", trees = trees_sorted)
 
 if __name__=="__main__":
     app.run(debug=True)
