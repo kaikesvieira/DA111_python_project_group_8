f9d99086ee247c625d0e1c2a9880e533c19f204e
kaikesvieira <kaikeincanada@gmail.com>
2024-04-11 12:36:44 -0400

    db_path modifications

--- Website/website.py
+++ Website/website.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template #create a server on your laptop
 import pathlib
 import sqlite3
 
-base_path = pathlib.Path('DA111_python_project_group_8')
+base_path = pathlib.Path(r'C:\Users\defaultuser0\OneDrive\Documents\GitHub\DA111_python_project_group_8\Database')
 db_name = "Trees.db"
 db_path = base_path / db_name
 print(db_path)
@@ -19,7 +19,7 @@ def about():
 
 @app.route("/data")
 def data():
-    con = sqlite3.connect('Trees.db')
+    con = sqlite3.connect(db_path)
     cursor = con.cursor()
     trees = cursor.execute("SELECT *, AVG(DBH) AS avg_dbh FROM trees GROUP BY Species ORDER BY avg_dbh DESC LIMIT 20").fetchall()
     trees_sorted = sorted(trees, key=lambda x: x[4], reverse=True)
